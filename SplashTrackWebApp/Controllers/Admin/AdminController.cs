e7d95e9c9490711af3d858af04f601d53baca694
Jack den Ouden <jack@ldam.nl>
2024-08-16 12:36:40 +0200

    Added out of the box authentication no customizations yet.

---
+++ SplashTrackWebApp/Controllers/Admin/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SplashTrackWebApp.Models;
+using System.Diagnostics;
+
+namespace SplashTrackWebApp.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SplashTrackWebApp/Controllers/Setup/SetupController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SplashTrackWebApp.Models;
+using System.Diagnostics;
+
+namespace SplashTrackWebApp.Controllers.Admin
+{
+    public class SetupController : Controller
+    {
+        private readonly ILogger<SetupController> _logger;
+
+        public SetupController(ILogger<SetupController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
